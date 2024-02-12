0f9698317c18d17d104e5eb1982d20c374bf73eb
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-02-12 16:30:10 +0530

    feat: change user password

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from '../middlewares/asyncHandler.middleware.js';
 import User from '../models/user.model.js';
 import AppError from '../utils/appError.utils.js';
 import cloudinary from 'cloudinary';
+
 import fs from 'fs';
 
 /**
@@ -158,4 +159,125 @@ export const userLogout = asyncHandler(async (req, res, next) => {
     success: true,
     message: 'User Logout successfully',
   });
+});
+
+/**
+ *
+ * TODO: send mail after password is changes successfully
+ * @ChangePassword
+ * @ROUTE @POST {{URL}}/api/v1/user/change-password
+ * @return message with password updated or changed
+ * @ACCESS private
+ *
+ */
+export const changePassword = asyncHandler(async (req, res, next) => {
+  // destructure password
+  const { oldPassword, newPassword } = req.body;
+  const { id } = req.user; // because of the middleware isLoggedIn
+
+  // check if the field are there or not
+  if (!oldPassword || !newPassword) {
+    return next(
+      new AppError('oldPassword and newPassword should be included', 400)
+    );
+  }
+
+  // find the user by ID and selecting the password
+  const user = await User.findById(id).select('+password');
+
+  // check user is present
+  if (!user) {
+    return next(new AppError('User not available with the provided ID', 400));
+  }
+
+  // check if the old password is correct
+  const isValidPassword = await user.comparePassword(oldPassword);
+
+  // throw if error if the old password is not valid
+  if (!isValidPassword) {
+    return next(new AppError('Invalid Old password provided', 400));
+  }
+
+  // setting the new password
+  user.password = newPassword;
+
+  // save it to the database
+  await user.save();
+
+  // setting the password undefined so that it wont get sent in the response
+  user.password = undefined;
+
+  // sent success message
+  res.status(200).json({
+    success: true,
+    message: 'User password updated successfully',
+  });
+});
+
+/**
+ *
+ * @forgotPassword
+ * @ROUTE @Post {{URL}}/api/v1/user/reset
+ * @return sent mail to the user email and reset a password
+ * @ACCESS public
+ *
+ */
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  // extract  email from the request body
+  const { email } = req.body;
+
+  // if no email send email is required
+  if (!email) {
+    return next(new AppError('Email is required', 400));
+  }
+
+  // finding the user via email
+  const user = await User.findOne({ email });
+
+  // if no user email found send the message email not found
+  if (!user) {
+    return next(new AppError('Email not registered', 400));
+  }
+
+  // generating reset token
+  const resetToken = await user.generatePasswordResetToken();
+
+  // saving the forgotPasswordToken to DB
+  await user.save();
+
+  // constructing a url to send the correct data
+  /**HERE
+   * req.protocol will send if http or https
+   * req.get('host') will get the hostname
+   * the rest is the route that we will create to verify if token is correct or not
+   */
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/user/reset/${resetToken}`;
+
+  // TODO: only the token can be sent to the user mail
+
+  // sent the resetPasswordUrl to the user email
+  const subject = 'Reset Password';
+  const message = `You can reset your password by clicking <a href=${resetPasswordUrl} target="_blank"> Reset your password</a>\n if the above link does not work for some reason then copy paste this link in new tab ${resetPasswordUrl}.\n if you have not request this, kindly ignore.`;
+
+  try {
+    await sendEmail(email, subject, message);
+
+    // if email sent successfully send the success response
+    res.status(200).json({
+      success: true,
+      message: `Reset password token has been sent to ${email} successfully`,
+    });
+  } catch (error) {
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordExpiry = undefined;
+
+    await user.save();
+
+    return next(
+      new AppError(error.message || 'Something went wrong, please try again'),
+      500
+    );
+  }
 });
