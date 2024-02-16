5d954fe409e9ea842af872de8683071998d28da2
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-02-16 14:57:31 +0530

    fix: delete order

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -87,6 +87,7 @@ export const deleteOrder = asyncHandler(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     message: 'Order deleted successfully',
+    order,
   });
 });
 
